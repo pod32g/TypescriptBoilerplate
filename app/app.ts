0ed03f8ae66a34b5b6f0e6b3a34ec90390e8bc22
Perez Salazar David Ivan (RBEI/EBS1.6.4-MX) <prd1ga@bosch.com>
2019-08-23 10:47:48 -0500

    First commit

---
+++ app/app.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+const app = express();
+
+app.get('/', (req: any, res: any) => {
+    res.send('Hello World');
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
